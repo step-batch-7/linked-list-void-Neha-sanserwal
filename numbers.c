46e94aa570687db441494e53da57b9193b9ebd19
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-05-14 15:05:49 +0530

    added prints for floats

--- numbers.c
+++ numbers.c
@@ -9,12 +9,19 @@ void display_int(List_ptr list)
     p_walk = p_walk->next;
   }
 }
+void display_float(List_ptr list)
+{
+  Node_ptr p_walk = list->first;
+  while (p_walk != NULL)
+  {
+    printf("%0.2f\n", *(float *)&p_walk->element);
+    p_walk = p_walk->next;
+  }
+}
+
 int main()
 {
-  int int_value = 1;
   Element element;
-  float float_value;
-  char char_value;
   List_ptr list = create_list();
   *(int *)&element = 1;
   add_to_list(list, element);
@@ -27,8 +34,22 @@ int main()
   printf("Element removed : %d\n", *(int *)&element);
   element = remove_from_end(list);
   printf("Element removed : %d\n", *(int *)&element);
-
   element = remove_at(list, 0);
   printf("Element removed : %d\n", *(int *)&element);
+
+  list = create_list();
+  *(float *)&element = 1.1;
+  add_to_list(list, element);
+  *(float *)&element = 2.1;
+  add_to_start(list, element);
+  *(float *)&element = 3.1;
+  insert_at(list, element, 1);
+  display_float(list);
+  element = remove_from_start(list);
+  printf("Element removed : %0.2f\n", *(float *)&element);
+  element = remove_from_end(list);
+  printf("Element removed : %0.2f\n", *(float *)&element);
+  element = remove_at(list, 0);
+  printf("Element removed : %0.2f\n", *(float *)&element);
   return 0;
 }
